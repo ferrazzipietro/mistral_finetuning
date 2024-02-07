24b230f920d3fc067077c8385730ac99c12da1b4
ferrazzipietro <92532181+ferrazzipietro@users.noreply.github.com>
2024-02-07 15:34:10 +0100

    Update config.py

--- config/config.py
+++ config/config.py
@@ -1,8 +1,22 @@
-DATASET_CHEKPOINT="ferrazzipietro/e3c-sentences" 
-TRAIN_LAYER = "it.layer1"
+from datetime import datetime
+from lora_params import r
 
-ADAPTERS_VERSION = "_v0.2"
+DATASET_CHEKPOINT="ferrazzipietro/e3c-sentences" 
+TRAIN_LAYER = "es.layer1"
 BASE_MODEL_CHECKPOINT="mistralai/Mistral-7B-Instruct-v0.2"
 model_name=BASE_MODEL_CHECKPOINT.split('/')[1]
-ADAPTERS_CHECKPOINT=f"ferrazzipietro/{model_name}_adapters_{TRAIN_LAYER}_{ADAPTERS_VERSION}" 
+ADAPTERS_CHECKPOINT=f"ferrazzipietro/{model_name}_adapters_{TRAIN_LAYER}_loraR{r}" 
 FT_MODEL_CHECKPOINT="ferrazzipietro/ft_tmp" 
+
+WANDB_PROJECT_NAME = f'finetune {model_name} {TRAIN_LAYER}'
+WANDB_RUN_NAME = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+class tmp():
+    def __init__(self):
+        self.BASE_MODEL_CHECKPOINT = BASE_MODEL_CHECKPOINT
+        self.DATASET_CHEKPOINT = DATASET_CHEKPOINT
+        self.TRAIN_LAYER = TRAIN_LAYER
+        self.FT_MODEL_CHECKPOINT = FT_MODEL_CHECKPOINT
+        self.WANDB_PROJECT_NAME = WANDB_PROJECT_NAME
+        self.WANDB_RUN_NAME = WANDB_RUN_NAME
+    
