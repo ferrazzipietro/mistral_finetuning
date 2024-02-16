362a17e786c402967c6c2c0cfea40ff0fb2975dd
ferrazzipietro <ferrazzi@labsrv8.math.unipd.it>
2024-02-16 15:18:04 +0100

    log __init__

---
+++ log/enlayer1_3epochs_4bits__ft_params.py
@@ -0,0 +1,68 @@
+DATASET_CHEKPOINT="ferrazzipietro/e3c-sentences" 
+BASE_MODEL_CHECKPOINT="mistralai/Mistral-7B-Instruct-v0.2"
+model_name=BASE_MODEL_CHECKPOINT.split('/')[1]
+
+TRAIN_LAYER = "en.layer1"
+ADAPTERS_CHECKPOINT= f"ferrazzipietro/{model_name}_adapters_{TRAIN_LAYER}"
+FT_MODEL_CHECKPOINT="ferrazzipietro/ft_tmp" 
+
+
+r = [16, 32, 64]
+lora_alpha = [32]
+lora_dropout = [0.05, 0.01]
+bias =  "lora_only" 
+use_rslora = True
+task_type="CAUSAL_LM"
+target_modules=["q_proj", "k_proj", "v_proj", "o_proj","gate_proj"]
+
+import torch
+
+
+load_in_4bit=[True]
+bnb_4bit_quant_type = ["nf4"]
+bnb_4bit_compute_dtype = [torch.bfloat16]
+llm_int8_threshold = [6.0]
+
+bnb_4bit_use_double_quant = True
+llm_int8_has_fp16_weight = True
+llm_int8_skip_modules = ["q_proj", "k_proj", "v_proj", "o_proj","gate_proj"]
+
+from transformers import AutoTokenizer
+
+task='finetuning'
+offset=False
+instruction_on_response_format='Return the result in a json format.'
+n_shots = 0
+tokenizer = AutoTokenizer.from_pretrained("mistralai/Mistral-7B-Instruct-v0.2")
+list_of_examples=[]
+list_of_responses=[]
+
+
+### TrainingArguments
+num_train_epochs= 3
+per_device_train_batch_size= 8
+gradient_accumulation_steps= [2,4,8]
+optim = "paged_adamw_8bit"
+save_steps= 1000
+logging_strategy="steps"
+logging_steps= 10
+learning_rate= [2e-4, 2e-3]
+weight_decay= 0.001
+fp16= True 
+bf16= False
+max_grad_norm= 0.3
+max_steps= -1
+warmup_ratio= 0.3
+group_by_length= True
+lr_scheduler_type= "constant"
+
+
+### SFTTrainer
+"""
+    max_seq_length - The maximum sequence length to use for the ConstantLengthDataset and for automatically creating the Dataset. Defaults to 512.
+    dataset_text_field - The name of the field containing the text to be used for the dataset. Defaults to "text".
+    packing - Used only in case dataset_text_field is passed. This argument is used by the ConstantLengthDataset to pack the sequences of the dataset.
+"""
+max_seq_length= 1024
+dataset_text_field="prompt"
+packing=False
