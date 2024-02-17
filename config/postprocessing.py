337be0ade78bd040b3fbb677b4b00a73f21b5008
ferrazzipietro <ferrazzipietro@gmail.com>
2024-02-17 12:27:43 +0100

    fixing size data

--- config/postprocessing.py
+++ config/postprocessing.py
@@ -2,5 +2,5 @@ max_new_tokens_factor_list = [4]
 n_shots_inference_list = [2]
 
 # EVALUATION
-similar_is_equal_list=[True]
-similar_is_equal_threshold_list=[70]
+similar_is_equal_list=[True, True, True, True, True, True, True, True, False]
+similar_is_equal_threshold_list=[95, 90, 85, 80, 75, 70, 65, 60, 100]
