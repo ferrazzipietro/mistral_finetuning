a91479de00454d8d83a4e8d047e041cda073d05e
ferrazzipietro <ferrazzipietro@gmail.com>
2024-02-27 13:58:24 +0100

    tmp

--- config/finetuning/config.py
+++ config/finetuning/config.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
 DATASET_CHEKPOINT="ferrazzipietro/e3c-sentences" 
-BASE_MODEL_CHECKPOINT="mistralai/Mistral-7B-Instruct-v0.2"
+BASE_MODEL_CHECKPOINT="mistralai/Mistral-7B-v0.1" # "mistralai/Mistral-7B-Instruct-v0.2"
 model_name=BASE_MODEL_CHECKPOINT.split('/')[1]
 
 TRAIN_LAYER = "en.layer1"
