1e0f9fed837dbe2c837b28fdc02bf5228f48932f
ferrazzipietro <ferrazzipietro@gmail.com>
2024-03-01 15:07:02 +0100

    qwen

---
+++ config/finetuning_qwen/preprocessing_params.py
@@ -0,0 +1,4 @@
+
+offset=False
+instruction_on_response_format='Extract the entities contained in the text and the offset, i.e. the position of that entity in the string. Extract only entities contained in the text.\nReturn the result in a json format: [{"entity":"entity_name"}].'
+simplest_prompt=False
