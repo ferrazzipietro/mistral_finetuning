a8fa80b86914658342b2de1059b5d371d6f1d4ab
ferrazzipietro <ferrazzi@labsrv8.math.unipd.it>
2024-03-06 21:17:20 +0100

    tmp

--- config/postprocessing.py
+++ config/postprocessing.py
@@ -1,5 +1,5 @@
-max_new_tokens_factor_list = [8]
-n_shots_inference_list = [0] #[2, 4]
+max_new_tokens_factor_list = [2,4,8]
+n_shots_inference_list = [0,2,4] #[2, 4]
 
 # EVALUATION
 similar_is_equal_list=[True, False]
