e2935b1838ec0341613dc6e60fe48356379b14af
ferrazzipietro <ferrazzi@labsrv8.math.unipd.it>
2024-03-07 18:49:46 +0100

    tmp

--- config/base_model_qwen.py
+++ config/base_model_qwen.py
@@ -1,6 +1,6 @@
 max_new_tokens_factor_list = [4,8]
 n_shots_inference_list = [0,1,2,3,4]
 TRAIN_LAYER = 'en.layer1'
-save_directory = 'data/qwen/14B_8bit_base' # "data/gemma/test_data_processed"
-BASE_MODEL_CHECKPOINT = "Qwen/Qwen1.5-14B-Chat"#"Qwen/Qwen1.5-14B-Chat"# "google/gemma-7b-it" # "mistralai/Mistral-7B-Instruct-v0.2"
+save_directory = 'data/qwen/7B_4bit_base' # "data/gemma/test_data_processed"
+BASE_MODEL_CHECKPOINT = "Qwen/Qwen1.5-7B-Chat"#"Qwen/Qwen1.5-14B-Chat"# "google/gemma-7b-it" # "mistralai/Mistral-7B-Instruct-v0.2"
 instruction_on_response_format = 'Extract the entities contained in the text.\nReturn the result in a json format: [{"entity":"entity_name"}].'
