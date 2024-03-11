61be317588ba54253181d7fe458db5f8597014d8
ferrazzipietro <ferrazzipietro@gmail.com>
2024-03-11 14:21:42 +0100

    tmp

--- config/postprocessing.py
+++ config/postprocessing.py
@@ -1,11 +1,11 @@
-max_new_tokens_factor_list = [4,8]
-n_shots_inference_list = [2,4]#[0,2,4] #[2, 4]
+max_new_tokens_factor_list = [2,4,8]
+n_shots_inference_list = [0,2,4]#[0,2,4] #[2, 4]
 
 # EVALUATION
 similar_is_equal_list=[True, False]
 similar_is_equal_threshold_list=[80, 100]#[95, 90, 85, 80, 75, 70, 65, 60, 100]
 words_level = True
-input_data_dir_path = 'data/mistral/4bit'
+input_data_dir_path = 'data/qwen/4bit'
 output_data_path = 'data/evaluation_results/mistral_4bit.csv'
 
 
