b41f78c3bd43b66ccacee5cfa0aa8fb18d2c81fb
ferrazzipietro <ferrazzi@labsrv8.math.unipd.it>
2024-03-13 14:16:58 +0100

    llama/13B_8bit_base

--- postprocessing_llama.py
+++ postprocessing_llama.py
@@ -83,7 +83,7 @@ for max_new_tokens_factor in max_new_tokens_factor_list:
             #try:
             postprocessor.add_responses_column(model=merged_model, 
                                             tokenizer=tokenizer, 
-                                            batch_size=12, 
+                                            batch_size=48, 
                                             max_new_tokens_factor=max_new_tokens_factor)
             postprocessor.test_data.to_csv(f"{postprocessing.save_directory}maxNewTokensFactor{max_new_tokens_factor}_nShotsInference{n_shots_inference}_{adapters.split('/')[1]}.csv", index=False)
             # except Exception as e:
