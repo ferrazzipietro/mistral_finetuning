fc1b9c26531bd93c3b2b7424ef1500ab9ba7d2f0
ferrazzipietro <ferrazzipietro@gmail.com>
2024-03-13 15:47:35 +0100

    tmp

--- get_results_base_model_qwen.py
+++ get_results_base_model_qwen.py
@@ -25,24 +25,37 @@ preprocessor = DataPreprocessor(model_checkpoint=base_model.BASE_MODEL_CHECKPOIN
 dataset = preprocessor.preprocess_data_one_layer(dataset, instruction_on_response_format=base_model.instruction_on_response_format,)
 _, val_data, _ = preprocessor.split_layer_into_train_val_test_(dataset, layer)
 
-bnb_config = BitsAndBytesConfig(
-            load_in_4bit=True,
-            load_in_8bit=False,
-            bnb_4bit_use_double_quant=True,
-            bnb_4bit_quant_type="nf4",
-            bnb_4bit_compute_dtype=torch.bfloat16,
-           # llm_int8_threshold= 6.0,
-           # llm_int8_skip_modules= ["q_proj", "k_proj", "v_proj", "o_proj","gate_proj"],
-            )
-model = AutoModelForCausalLM.from_pretrained(
-            base_model.BASE_MODEL_CHECKPOINT, low_cpu_mem_usage=True,
-            quantization_config = bnb_config,
-            return_dict=True, 
-            #torch_dtype=torch.float16,
-            device_map= "auto",
-            token=HF_TOKEN,
-            #cache_dir='/data/disk1/share/pferrazzi/.cache'
-            )
+
+load_in_4bit = False
+load_in_4bit = False
+if base_model.n_bit==4:
+    load_in_4bit = True
+if base_model.n_bit==8:
+    load_in_8bit = True
+
+if load_in_4bit or load_in_8bit: 
+    bnb_config = BitsAndBytesConfig(
+                load_in_4bit=load_in_4bit,
+                load_in_8bit=load_in_8bit,
+                bnb_4bit_use_double_quant=True,
+                bnb_4bit_quant_type="nf4",
+                bnb_4bit_compute_dtype=torch.bfloat16,
+                llm_int8_threshold= 6.0,
+                llm_int8_skip_modules= ["q_proj", "k_proj", "v_proj", "o_proj","gate_proj"],
+                )
+    model = AutoModelForCausalLM.from_pretrained(
+                base_model.BASE_MODEL_CHECKPOINT, low_cpu_mem_usage=True,
+                quantization_config = bnb_config,
+                return_dict=True, 
+                #torch_dtype=torch.float16,
+                device_map= "auto",
+                token=HF_TOKEN,
+                #cache_dir='/data/disk1/share/pferrazzi/.cache'
+    )
+else:
+    model = AutoModelForCausalLM.from_pretrained(base_model.BASE_MODEL_CHECKPOINT, low_cpu_mem_usage=True,
+                                                return_dict=True, device_map= "auto", token=HF_TOKEN)
+
 tokenizer = AutoTokenizer.from_pretrained(base_model.BASE_MODEL_CHECKPOINT, add_eos_token=True, token=HF_TOKEN)
 tokenizer.pad_token = '<pad>'
 tokenizer.padding_side = "left"
