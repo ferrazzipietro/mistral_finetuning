899f11dbedd4aa5ac97ebecca087429d76f147ea
ferrazzipietro <ferrazzipietro@gmail.com>
2024-03-14 11:25:03 +0100

    tmp

--- log/enlayer1_3epochs_4bits__ft_params_mistral.py
+++ log/enlayer1_3epochs_4bits__ft_params_mistral.py
@@ -16,8 +16,9 @@ task_type="CAUSAL_LM"
 target_modules=["q_proj", "k_proj", "v_proj", "o_proj","gate_proj"]
 
 import torch
+quantization=True
 
-
+simplest_prompt = False
 load_in_4bit=[True]
 bnb_4bit_quant_type = ["nf4"]
 bnb_4bit_compute_dtype = [torch.bfloat16]
