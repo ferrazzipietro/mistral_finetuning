111b89e3ea70e5527dace78295458d570981c78c
ferrazzipietro <ferrazzipietro@gmail.com>
2024-03-19 09:41:59 +0100

    tmp

--- config/postprocessing_params_zefiro.py
+++ config/postprocessing_params_zefiro.py
@@ -2,5 +2,5 @@ import torch
 torch_dtype = torch.float16
 max_new_tokens_factor_list = [4,8]
 n_shots_inference_list = [0, 2, 4] # [0,2,4] #
-save_directory = 'data/llama/7B_NoQuant_FT/'
+save_directory = 'data/zefiro/4bit_FT/'
 batch_size = 48
--- evaluation.py
+++ evaluation.py
@@ -3,9 +3,7 @@ from utils.evaluator import Evaluator
 from utils.output_cleaner import OutputCleaner
 import glob
 import pandas as pd
-#adapters_list = generate_ft_adapters_list("enlayer1_3epochs_4bits__ft_params")
 
-    
 def evaluate(input_data_path:str, 
              output_file_path:str, 
              words_level:bool,
@@ -34,17 +32,17 @@ def evaluate(input_data_path:str,
     for file in csv_files:
         print("FILE: " , file)
         eval_data = Dataset.from_csv(file) 
-        output_cleaner = OutputCleaner(verbose=False)
-        cleaned_data = output_cleaner.apply_cleaning(eval_data, wrong_keys_to_entity=wrong_keys_to_entity)
-        for similar_is_equal_threshold in similar_is_equal_threshold_list:
-            evaluator = Evaluator(data=cleaned_data, offset=offset, output_cleaner=output_cleaner)
-            try:
+        try:
+            output_cleaner = OutputCleaner(verbose=False)
+            cleaned_data = output_cleaner.apply_cleaning(eval_data, wrong_keys_to_entity=wrong_keys_to_entity)
+            for similar_is_equal_threshold in similar_is_equal_threshold_list:
+                evaluator = Evaluator(data=cleaned_data, offset=offset, output_cleaner=output_cleaner)
                 evaluator.generate_evaluation_table(similar_is_equal_threshold=similar_is_equal_threshold,
                                                     words_level=words_level, 
                                                     similarity_types=similarity_types)
                 evaluation_results.loc[len(evaluation_results)] = {'file': file, 'similar_is_equal_threshold': similar_is_equal_threshold, 'f1_score': evaluator.evaluation_table['f1'], 'precision': evaluator.evaluation_table['precision'], 'recall': evaluator.evaluation_table['recall']}
-            except Exception as e:
-                errors.extend({file: e})
+        except Exception as e:
+            errors.extend({file: e})
     evaluation_results.to_csv(output_file_path, index=False)
     return errors
 
