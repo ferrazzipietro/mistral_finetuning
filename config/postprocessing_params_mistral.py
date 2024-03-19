f9725c0c114ce784197742823c6b6cc83a12b458
ferrazzipietro <ferrazzi@labsrv8.math.unipd.it>
2024-03-19 13:18:36 +0100

    tmp

--- config/postprocessing_params_mistral.py
+++ config/postprocessing_params_mistral.py
@@ -1,8 +1,8 @@
 import torch
 max_new_tokens_factor_list = [4,8]
-n_shots_inference_list = [0,2,4] #[2, 4]
+n_shots_inference_list = [2,4] #[2, 4]
 save_directory = 'data/mistral/NoQuant_FT/'
-batch_size = 12
+batch_size = 4
 torch_dtype=torch.float32
 
 # EVALUATION
