68e213947dc36f53be3a460e9fd3adf7fbfd9448
ferrazzipietro <ferrazzipietro@gmail.com>
2024-04-08 17:26:11 +0200

    tmp

--- config/postprocessing_params_qwen.py
+++ config/postprocessing_params_qwen.py
@@ -1,6 +1,6 @@
 max_new_tokens_factor_list = [2,4,8]
-n_shots_inference_list = [0,2,4] #[2, 4]
+n_shots_inference_list = [2,4] #[0, 2, 4]
 save_directory = 'data/qwen/7B_8bit_FT/'
-batch_size = 36
+batch_size = 12
 
 
