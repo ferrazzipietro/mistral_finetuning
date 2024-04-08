822a50ddf750169b8ad0dff97c80fc2a7bb5a411
ferrazzipietro <ferrazzipietro@gmail.com>
2024-04-08 11:40:54 +0200

    llama

--- config/postprocessing_params_qwen.py
+++ config/postprocessing_params_qwen.py
@@ -1,17 +1,6 @@
 max_new_tokens_factor_list = [2,4,8]
 n_shots_inference_list = [0,2,4] #[2, 4]
 save_directory = 'data/qwen/7B_8bit_FT/'
-batch_size = 12
+batch_size = 36
 
-# EVALUATION
-similar_is_equal_list=[True, False]
-similar_is_equal_threshold_list=[100]#[95, 90, 85, 80, 75, 70, 65, 60, 100]
-words_level = True
-input_data_dir_path = 'data/qwen/4bit'
-output_data_path = 'data/evaluation_results/mistral_4bit.csv'
-
-
-similarity_types = ['case', 'stop_words', 'subset', 'superset', 'levenshtein']
-wrong_keys_to_entity = False
-offset = False
 
