4e8342e9c5109430e11c113da338a686ba5c54b2
ferrazzipietro <ferrazzipietro@gmail.com>
2024-04-08 10:10:51 +0200

    tmp

--- config/postprocessing_params_zefiro.py
+++ config/postprocessing_params_zefiro.py
@@ -1,6 +1,6 @@
 import torch
 torch_dtype = torch.float16
 max_new_tokens_factor_list = [4,8]
-n_shots_inference_list = [0, 2] # [0,2,4] #
-save_directory = 'data/zefiro/4bit_FT/'
-batch_size = 48
+n_shots_inference_list = [0, 2, 4] # [0,2,4] #
+save_directory = 'data/zefiro/noQuant_FT/'
+batch_size = 36
