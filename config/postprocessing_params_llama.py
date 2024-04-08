33c857c28738888af2574fda5ec0f50208fe9122
ferrazzipietro <ferrazzipietro@gmail.com>
2024-04-08 17:14:22 +0200

    tmp

--- config/postprocessing_params_llama.py
+++ config/postprocessing_params_llama.py
@@ -3,4 +3,4 @@ torch_dtype = torch.float16
 max_new_tokens_factor_list = [4,8]
 n_shots_inference_list = [0, 2, 4] # [0,2,4] #
 save_directory = 'data/llama/13B_8bit_FT/'
-batch_size = 48
+batch_size = 12
