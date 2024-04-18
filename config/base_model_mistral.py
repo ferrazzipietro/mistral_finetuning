216454848ae9a2aaf271c8fc5e023db41fd6d202
ferrazzipietro <ferrazzipietro@gmail.com>
2024-04-18 17:37:45 +0200

    tmp

--- config/base_model_mistral.py
+++ config/base_model_mistral.py
@@ -1,9 +1,9 @@
-max_new_tokens_factor_list = [2, 4,8]# [2,4,8]
+max_new_tokens_factor_list = [2,4,8]# [2,4,8]
 n_shots_inference_list = [0,1,2,3,4]
 TRAIN_LAYER = 'en.layer1'
 
 n_bit=None
-save_directory = "data/mistral/NoQuant_base" # f"data/mistral/{n_bit}bit_base" 
+save_directory = "data/mistral/4bit_FT" # f"data/mistral/{n_bit}bit_base" 
 BASE_MODEL_CHECKPOINT = "mistralai/Mistral-7B-Instruct-v0.2"# "google/gemma-7b-it" # "mistralai/Mistral-7B-Instruct-v0.2"
 instruction_on_response_format = 'Extract the entities contained in the text.\nReturn the result in a json format: [{"entity":"entity_name"}].'
 simplest_prompt = False
