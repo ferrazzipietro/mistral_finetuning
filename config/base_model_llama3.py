be25c9b55b6d55db4f41f75c0f0c73d9a269141d
ferrazzipietro <ferrazzipietro@gmail.com>
2024-04-19 15:21:06 +0200

    tmp

--- config/base_model_llama3.py
+++ config/base_model_llama3.py
@@ -4,7 +4,7 @@ max_new_tokens_factor_list = [2,4,8]
 n_shots_inference_list = [0,1,2,3,4]
 TRAIN_LAYER = 'en.layer1'
 
-n_bit='NoQuant'
+n_bit=4 # 'NoQuant'
 save_directory = f"data/llama3/13B_{n_bit}bit_base" # "data/gemma/test_data_processed"
 BASE_MODEL_CHECKPOINT = "meta-llama/Meta-Llama-3-8B-Instruct"# "google/gemma-7b-it" # "mistralai/Mistral-7B-Instruct-v0.2"
 
@@ -12,4 +12,4 @@ instruction_on_response_format = 'Extract the entities contained in the text.\nR
 simplest_prompt = False
 
 torch_dtype = torch.bfloat16
-batch_size = 24
+batch_size = 20
