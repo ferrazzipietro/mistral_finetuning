98602d07ab1bc2af27cf3324db70b4fd8361bd48
ferrazzipietro <ferrazzipietro@gmail.com>
2024-04-23 09:29:24 +0200

    remove llama_NoQuant_FT old

--- config/evaluation_params_all.py
+++ config/evaluation_params_all.py
@@ -7,6 +7,6 @@ offset = False
 
 
 # params for evaluation.py, not all
-input_data_dir_path = 'data/llama3/8B_4bit_base' # data/qwen/7B_NoQuantbit_base
-output_data_path = 'data/evaluation_results/llama3_8B_4bit_base.csv'#qwen_7B_NoQuant_base
+input_data_dir_path = 'data/llama/7B_8bit_FT' # data/qwen/7B_NoQuantbit_base
+output_data_path = 'data/evaluation_results/llama_7B_8bit_FT.csv'#qwen_7B_NoQuant_base
 
