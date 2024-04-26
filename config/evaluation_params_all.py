1d7c481f2ca828d3f0f7033b1f1de32745c53bab
ferrazzipietro <ferrazzipietro@gmail.com>
2024-04-26 15:56:33 +0200

    tmp

--- config/evaluation_params_all.py
+++ config/evaluation_params_all.py
@@ -7,6 +7,6 @@ offset = False
 
 
 # params for evaluation.py, not all
-input_data_dir_path = 'data/zefiro/4bit_FT' # 'data/phi3/4bit_base' # data/qwen/7B_NoQuantbit_base
-output_data_path = 'data/evaluation_results/zefiro_4bit_FT' # 'data/evaluation_results/phi3_4bit_base.csv'#qwen_7B_NoQuant_base
+input_data_dir_path = 'data/mistral/4bit_base' # 'data/phi3/4bit_base' # data/qwen/7B_NoQuantbit_base
+output_data_path = 'data/evaluation_results/mistral_4bit_base.csv' # 'data/evaluation_results/phi3_4bit_base.csv'#qwen_7B_NoQuant_base
 
