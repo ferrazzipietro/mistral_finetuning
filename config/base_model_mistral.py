c1f37ed78990b5d2883d922959b8ca2d719ce28d
ferrazzipietro <ferrazzipietro@gmail.com>
2024-04-26 15:44:47 +0200

    tmp

--- config/base_model_mistral.py
+++ config/base_model_mistral.py
@@ -3,7 +3,7 @@ n_shots_inference_list = [0,1,2,3,4]
 TRAIN_LAYER = 'en.layer1'
 
 n_bit=8
-save_directory = "data/mistral/8bit_base_bfloat16" # f"data/mistral/{n_bit}bit_base" 
+save_directory = "data/mistral/NoQuant_base_bfloat16" # f"data/mistral/{n_bit}bit_base" 
 BASE_MODEL_CHECKPOINT = "mistralai/Mistral-7B-Instruct-v0.2"# "google/gemma-7b-it" # "mistralai/Mistral-7B-Instruct-v0.2"
 instruction_on_response_format = 'Extract the entities contained in the text.\nReturn the result in a json format: [{"entity":"entity_name"}].'
 simplest_prompt = False
