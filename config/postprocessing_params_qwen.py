1c6ce0eeae4ec3a803cb05445367827c97eec4ed
ferrazzipietro <ferrazzipietro@gmail.com>
2024-04-26 15:19:27 +0200

    qwen

--- config/postprocessing_params_qwen.py
+++ config/postprocessing_params_qwen.py
@@ -3,6 +3,6 @@ import torch
 max_new_tokens_factor_list = [2,4,8]
 n_shots_inference_list = [0, 2, 4] #[0, 2, 4]
 save_directory = 'data/qwen/7B_NoQuant_FT/'
-batch_size = 12
+batch_size = 6
 torch_dtype=torch.float16
 
