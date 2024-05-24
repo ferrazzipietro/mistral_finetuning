19f8c4b2b84759f22b6e6e4ac94e8134d61abb33
PietroFerr <ferrazzipietro@gmail.com>
2024-05-24 19:32:40 +0200

    tmp

---
+++ config/finetuning_llama2_slovenian/config.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+from .preprocessing_params import simplest_prompt
+
+
+DATASET_CHEKPOINT="ferrazzipietro/e3c-sentences" 
+BASE_MODEL_CHECKPOINT="meta-llama/Llama-2-7b-chat-hf" # "meta-llama/Llama-2-7b-chat-hf"  # 
+model_name=BASE_MODEL_CHECKPOINT.split('/')[1]
+
+TRAIN_LAYER = "en.layer1"
+ADAPTERS_CHECKPOINT= f"ferrazzipietro/{model_name}_adapters_{TRAIN_LAYER}"
+FT_MODEL_CHECKPOINT="ferrazzipietro/ft_tmp" 
+if simplest_prompt:
+    ADAPTERS_CHECKPOINT=ADAPTERS_CHECKPOINT + "_simplest_prompt"
+
+WANDB_PROJECT_NAME = f'finetune {model_name} {TRAIN_LAYER}'
+WANDB_RUN_NAME = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+
+slovenian_train_path = 'mistral_finetuning/data/slovenian/E3C_Slovenian_Train_SL_L1.csv'
+slovenian_test_path = 'mistral_finetuning/data/slovenian/E3C_Slovenian_Test_SL_L1.csv'
---
+++ config/finetuning_llama2_slovenian/training_params.py
@@ -0,0 +1,28 @@
+### TrainingArguments
+num_train_epochs= 3
+per_device_train_batch_size= 8
+gradient_accumulation_steps= [2]# ,4]#[2,4,8] reduce the number to finish faster
+optim = "paged_adamw_8bit"
+save_steps= 1000
+logging_strategy="steps"
+logging_steps= 10
+learning_rate= [2e-4]
+weight_decay= 0.001
+fp16= True 
+bf16= False
+max_grad_norm= 0.3
+max_steps= -1
+warmup_ratio= 0.3
+group_by_length= True
+lr_scheduler_type= "constant"
+
+
+### SFTTrainer
+"""
+    max_seq_length - The maximum sequence length to use for the ConstantLengthDataset and for automatically creating the Dataset. Defaults to 512.
+    dataset_text_field - The name of the field containing the text to be used for the dataset. Defaults to "text".
+    packing - Used only in case dataset_text_field is passed. This argument is used by the ConstantLengthDataset to pack the sequences of the dataset.
+"""
+max_seq_length= 1024
+dataset_text_field="prompt"
+packing=False
