1eab33fc97586ef21e3c972364f0beb4954ca3ef
PietroFerr <ferrazzipietro@gmail.com>
2024-05-24 19:43:31 +0200

    tmp

--- slovenian_finetuning_iterative_llama2.py
+++ slovenian_finetuning_iterative_llama2.py
@@ -28,8 +28,8 @@ def main(ADAPTERS_CHECKPOINT,
   
   # Monitering the LLM
   wandb.login(key = WANDB_KEY)
-  run = wandb.init(project=ADAPTERS_CHECKPOINT.split('/')[1], job_type="training", anonymous="allow",
-                  name=config.TRAIN_LAYER+datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+  run = wandb.init(project=config.WANDB_PROJECT_NAME, job_type="training", anonymous="allow",
+                  name=ADAPTERS_CHECKPOINT.split('/')[1],
                   config={'model': config.BASE_MODEL_CHECKPOINT, 
                           'dataset': config.DATASET_CHEKPOINT, 
                           'layer': config.TRAIN_LAYER,
