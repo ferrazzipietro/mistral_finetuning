3b3f6dd08081894661fa78fc86aaef0d89a39f7d
ferrazzipietro <ferrazzipietro@gmail.com>
2024-06-03 15:47:07 +0200

    tmp

--- config/finetuning_llama2/training_params.py
+++ config/finetuning_llama2/training_params.py
@@ -1,6 +1,6 @@
 ### TrainingArguments
 ### TrainingArguments
-num_train_epochs= 5
+num_train_epochs= 3
 per_device_train_batch_size= 16
 gradient_accumulation_steps= [1]#[2,4,8] # reduce the number to finish faster
 optim = "paged_adamw_8bit"
@@ -16,14 +16,14 @@ lr_scheduler_type= "constant"
 
 logging_steps=2
 logging_strategy="steps"
-evaluation_strategy= "steps"
+evaluation_strategy= "epochs"#"steps"
 save_strategy=evaluation_strategy
-save_steps= 25
+save_steps= -1
 eval_steps=save_steps
 greater_is_better=False
 metric_for_best_model="eval_loss"
 save_total_limit = 1
-load_best_model_at_end = True
+load_best_model_at_end = False
 
 ### SFTTrainer
 """
