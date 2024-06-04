c3cf6e3177f4bebedb54a118c5fc8ae5a4fab6a8
ferrazzipietro <ferrazzipietro@gmail.com>
2024-06-04 15:15:57 +0200

    tmp

--- config/postprocessing_params_llama.py
+++ config/postprocessing_params_llama.py
@@ -2,6 +2,6 @@ import torch
 torch_dtype = torch.bfloat16
 max_new_tokens_factor_list = [4,6,8, 10] #,6,8] # [8]
 n_shots_inference_list = [0] # [0,2,4] # [0] # 
-save_directory = 'data/llama/7B_NoQuant_FT_cl_v2prompt/' # 'data/llama/7B_NoQuant_FT/'
+save_directory = 'data/llama/7B_NoQuant_FT_cl_1epoch/' # 'data/llama/7B_NoQuant_FT/'
 batch_size = 48# 1
 temperature = 0.2
