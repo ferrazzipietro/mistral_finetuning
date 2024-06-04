cf6c80824e4a0b85a4e608e54d429f15fb85630b
ferrazzipietro <ferrazzipietro@gmail.com>
2024-06-04 10:44:56 +0200

    tmp

--- config/postprocessing_params_llama.py
+++ config/postprocessing_params_llama.py
@@ -4,4 +4,4 @@ max_new_tokens_factor_list = [4,6,8] #,6,8] # [8]
 n_shots_inference_list = [0] # [0,2,4] # [0] # 
 save_directory = 'data/llama/7B_NoQuant_FT_cl_v2prompt/' # 'data/llama/7B_NoQuant_FT/'
 batch_size = 48# 1
-temperature = 0.2
+temperature = 0.8
