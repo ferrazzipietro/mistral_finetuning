aa266deac082491d53bf4ce0e0975571ab018eb2
ferrazzipietro <ferrazzipietro@gmail.com>
2024-06-04 15:24:13 +0200

    tmp

--- log/llama_7B_NoQuant_1epoch.py
+++ log/llama_7B_NoQuant_1epoch.py
@@ -8,8 +8,6 @@ model_name=BASE_MODEL_CHECKPOINT.split('/')[1]
 TRAIN_LAYER = "en.layer1"
 ADAPTERS_CHECKPOINT= f"ferrazzipietro/{model_name}_adapters_{TRAIN_LAYER}"
 FT_MODEL_CHECKPOINT="ferrazzipietro/ft_tmp" 
-if simplest_prompt:
-    ADAPTERS_CHECKPOINT=ADAPTERS_CHECKPOINT + "_simplest_prompt"
 
 WANDB_PROJECT_NAME = f'finetune {model_name} {TRAIN_LAYER}'
 WANDB_RUN_NAME = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
